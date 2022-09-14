1ab731b02a57ed70e40011316d5eb12f46fa259a
19523115 <19523115@students.uii.ac.id>
2022-09-14 15:23:31 +0700

    problem-app-bar

---
+++ src/script/view/main.js
@@ -0,0 +1,54 @@
+import DataSource from "../data/data-source";
+
+const main = () => {
+  const searchElement = document.querySelector('#searchElement');
+  const buttonSearchElement = document.querySelector('#searchButtonElement');
+  const clubListElement = document.querySelector('#clubList');
+
+  // menggunakan menggunakan method biasa
+  // const onButtonSearchClicked = () => {
+  //   DataSource.searchClub(searchElement.value)
+  //     .then(renderResult)
+  //     .catch(fallbackResult)
+  //   // const dataSource = new DataSource(renderResult, fallbackResult);
+  //   // dataSource.searchClub(searchElement.value);
+  // };
+
+  // menggunakan async/await
+
+  const onButtonSearchClicked = async () => {
+    try {
+      const result = await DataSource.searchClub(searchElement.value)
+      renderResult(result);
+    } catch (message) {
+      fallbackResult(message)
+    }
+  };
+
+  const renderResult = results => {
+    clubListElement.innerHTML = '';
+    results.forEach(club => {
+      const { name, fanArt, description } = club;
+
+      const clubElement = document.createElement('div');
+      clubElement.setAttribute('class', 'club');
+      clubElement.innerHTML = `
+        < img class="fan-art-club" src = "${fanArt}" alt = "Fan Art" >
+          <div class="club-info">
+            <h2> ${name} </h2>
+            <p> ${description} </p>
+          </div>
+      `;
+      clubListElement.appendChild(clubElement);
+    });
+  };
+
+  const fallbackResult = message => {
+    clubListElement.innerHTML = '';
+    clubListElement.innerHTML += `<h2 class="placeholder"> ${message} </h2>`;
+  };
+
+  buttonSearchElement.addEventListener('click', onButtonSearchClicked);
+};
+
+export default main;
